eb87a8c81b19bb4da12691ed91705e818b573413
carter-dm <cdalymac@gmail.com>
2019-08-07 09:50:57 -0230

    Fix clunky movement

--- src/main/java/actors/Car.java
+++ src/main/java/actors/Car.java
@@ -7,12 +7,16 @@ package actors;
 
 import game.Stage;
 import java.awt.event.KeyEvent;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
  * @author Eric
  */
 public class Car extends Actor implements KeyboardControllable {
+
+    Map<String, Boolean> keysPressed = new HashMap<String, Boolean>();
 
     public enum ePlayerNumber {
         PN_ONE,
@@ -22,18 +26,25 @@ public class Car extends Actor implements KeyboardControllable {
     private ePlayerNumber playerNumber;
 
     public Car(Stage canvas, ePlayerNumber playerNo) {
-        super(canvas, 125,56, 125,56);
+        super(canvas, 160,160, 160,160);
         playerNumber = playerNo;
-        sprites = new String[]{"car.png"};
+        sprites = new String[]{"Car_Idle.png"};
         frame = 0;
         posX = Stage.WIDTH / 2 - getWidth() / 2;
         posY = Stage.HEIGHT - getHeight() * 2;
+
+        setKeys();
     }
 
     public void update() {
         super.update();
-        posX += vx * 3;
-        posY += vy * 3;
+        updatePlayerCoords();
+
+        posX += vx;
+        posY += vy;
+
+        vx *= 0.9;
+        vy *= 0.9;
 
         if( posX > Stage.WIDTH - getWidth() ) {
             posX = Stage.WIDTH - getWidth();
@@ -43,7 +54,7 @@ public class Car extends Actor implements KeyboardControllable {
         }
 
         if( posY > Stage.HEIGHT - getHeight() ) {
-            posY = Stage.HEIGHT - getHeight() * 2;
+            posY = Stage.HEIGHT - getHeight();
         }
         else if( posY < 0 ) {
             posY = 0;
@@ -53,30 +64,67 @@ public class Car extends Actor implements KeyboardControllable {
     @Override
     public void triggerKeyPress(KeyEvent e) {
         if( e.getKeyCode() == KeyEvent.VK_D) {
-            vx = 1;
+            keysPressed.put("RIGHT", true);
         }
-        else if( e.getKeyCode() == KeyEvent.VK_A) {
-            vx = -1;
+        if( e.getKeyCode() == KeyEvent.VK_A) {
+            keysPressed.put("LEFT", true);
         }
-        else if( e.getKeyCode() == KeyEvent.VK_S) {
-            vy = 1;
+        if( e.getKeyCode() == KeyEvent.VK_S) {
+            keysPressed.put("DOWN", true);
         }
-        else if( e.getKeyCode() == KeyEvent.VK_W) {
-            vy = -1;
+        if( e.getKeyCode() == KeyEvent.VK_W) {
+            keysPressed.put("UP", true);
         }
     }
 
     @Override
     public void triggerKeyRelease(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_D) {
-            vx = 0;
-        } else if (e.getKeyCode() == KeyEvent.VK_A) {
-            vx = 0;
+            keysPressed.put("RIGHT", false);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_A) {
+            keysPressed.put("LEFT", false);
         }
         if (e.getKeyCode() == KeyEvent.VK_S) {
-            vy = 0;
-        } else if (e.getKeyCode() == KeyEvent.VK_W) {
-            vy = 0;
+            keysPressed.put("DOWN", false);
+        }
+        if (e.getKeyCode() == KeyEvent.VK_W) {
+            keysPressed.put("UP", false);
+        }
+    }
+
+    private void setKeys() {
+        keysPressed.put("UP", false);
+        keysPressed.put("LEFT", false);
+        keysPressed.put("DOWN", false);
+        keysPressed.put("RIGHT", false);
+    }
+
+
+    private void updatePlayerCoords() {
+        if (keysPressed.get("UP")) {
+            vy -= 1;
+        }
+        if (!keysPressed.get("UP")) {
+            vy += 1.5;
+        }
+        if (keysPressed.get("LEFT")) {
+            vx -= 2;
+        }
+        if (!keysPressed.get("LEFT")) {
+            vx += 1.5;
+        }
+        if (keysPressed.get("DOWN")) {
+            vy += 1;
+        }
+        if (!keysPressed.get("DOWN")) {
+            vy -= 1.5;
+        }
+        if (keysPressed.get("RIGHT")) {
+            vx += 2;
+        }
+        if (!keysPressed.get("RIGHT")) {
+            vx -= 1.5;
         }
     }
 }
