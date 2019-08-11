9862a627f574771f6290f9b1189b5d973b21978b
Carter-DM <cdalymac@gmail.com>
2019-08-11 19:33:20 -0230

    Add main menu

--- src/main/java/game/Stage.java
+++ src/main/java/game/Stage.java
@@ -18,6 +18,8 @@ public class Stage extends Canvas implements ImageObserver {
 	protected boolean gameWon = false;
 	protected boolean gameOver = false;
 	protected boolean paused = false;
+	protected boolean mainMenu = false;
+
 	public List<Actor> actors = new ArrayList<Actor>();	
 	
 	public Stage() {
@@ -34,6 +36,18 @@ public class Stage extends Canvas implements ImageObserver {
 	public void endGame() {
 		gameOver = true;
 	}
+
+	public void displayMainScreen() {
+	    mainMenu = true;
+    }
+
+    public void hideMainMenu() {
+	    mainMenu = false;
+    }
+
+    public boolean isMainMenuDisplaying() {
+	    return mainMenu;
+    }
 	
 	public boolean isGameOver() {
 		return gameOver;
